78d8ea6b925de09a37ac6970408e0f3304a77663
easybro1235 <easybro1235@kookmin.ac.kr>
2026-02-18 14:38:43 +0900

    2026/02/18 study

---
+++ templates/code3_1406.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// BOJ 1406
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    list<char> a;
+    char input, input2;
+    int num;
+    list<char>::iterator cursor = a.begin();
+    string first_input;
+    cin >> first_input;
+    for (auto ch : first_input) {
+        a.push_back(ch);
+    }
+    cursor = a.end();
+
+    cin >> num;
+    while (num--) {
+        cin >> input;
+        if (input == 'L') {
+            if (cursor != a.begin()) cursor--;
+        } else if (input == 'D') {
+            if (cursor != a.end()) cursor++;
+        } else if (input == 'B') {
+            if (cursor != a.begin()) {
+                --cursor;
+                cursor = a.erase(cursor);
+            }
+        } else if (input == 'P') {
+            cin >> input2;
+            a.insert(cursor, input2);
+            cursor++;
+        }
+    }
+
+    for (auto k : a) {
+        cout << k;
+    }
+
+#ifndef ONLINE_JUDGE
+    cout << endl; // 출력 버퍼를 비우고 줄바꿈 (필수!)
+    system("pause");
+#endif
+    return 0;
+}
